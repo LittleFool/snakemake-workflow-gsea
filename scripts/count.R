9842340c1057ae9e37b553a89666a07fa5dd733a
LittleFool <littlefool@fassiburg.de>
2020-04-09 19:31:58 +0200

    added paired-end config variable for featureCounts

--- scripts/count.R
+++ scripts/count.R
@@ -7,7 +7,7 @@ library(Rsubread)
 
 input <- snakemake@input
 vectorInput <- unlist(input)
-fc <- featureCounts(files=c(vectorInput),annot.ext=snakemake@config[["annotation"]],isGTFAnnotationFile=TRUE,GTF.featureType="exon",GTF.attrType="gene_id",nthreads=snakemake@threads)
+fc <- featureCounts(files=c(vectorInput),annot.ext=snakemake@config[["annotation"]],isGTFAnnotationFile=TRUE,GTF.featureType="exon",GTF.attrType="gene_id",nthreads=snakemake@threads,isPairedEnd=snakemake@config[["counting"]][["isPairedEnd"]])
 tsv <- cbind(fc[["annotation"]]["GeneID"], fc[["counts"]])
 
 samples = read.table(snakemake@config[["samples"]], header=TRUE, stringsAsFactors = FALSE)
